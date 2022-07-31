bb9d84624d1d766ddcb3e9fcae3289d3f0c8ee17
David Vaughan <david@davidv.xyz>
2022-07-31 09:25:42 -0700

    Clean up cab processing code

--- Main.gd
+++ Main.gd
@@ -4,19 +4,15 @@ var cab_state = null
 
 func _ready():
 	pass
-	
+
 func _process(delta):
-	var cab_frames = $cabs/AnimatedSprite.frames.get_animation_names()
-	if cab_state != null:
-		$cabs/AnimatedSprite.animation = cab_frames[cab_state]
-	else:
-		$cabs/AnimatedSprite.animation = cab_frames[6]
+	var cab_pics = $cabs/AnimatedSprite.frames.get_animation_names()
+	var cab_idx = cab_state if cab_state != null else 6
+	$cabs/AnimatedSprite.animation = cab_pics[cab_idx]
 
 func cab_open(num):
-	print("IN ", num)
 	cab_state = num
 
 func cab_close(num):
-	print("OUT ", num)
 	if cab_state == num:
 		cab_state = null
