86bf07011308da8ad14d20f005dca05ae8f502af
David Vaughan <david@davidv.xyz>
2022-07-31 09:20:37 -0700

    Fix cab-not-opening problem

--- Main.gd
+++ Main.gd
@@ -1,15 +1,22 @@
 extends Node
 
+var cab_state = null
 
 func _ready():
 	pass
+	
+func _process(delta):
+	var cab_frames = $cabs/AnimatedSprite.frames.get_animation_names()
+	if cab_state != null:
+		$cabs/AnimatedSprite.animation = cab_frames[cab_state]
+	else:
+		$cabs/AnimatedSprite.animation = cab_frames[6]
 
 func cab_open(num):
 	print("IN ", num)
-	var cab_frames = $cabs/AnimatedSprite.frames.get_animation_names()
-	$cabs/AnimatedSprite.animation = cab_frames[num]
+	cab_state = num
 
-func cab_close():
-	print("OUT")
-	var cab_frames = $cabs/AnimatedSprite.frames.get_animation_names()
-	$cabs/AnimatedSprite.animation = cab_frames[6]
+func cab_close(num):
+	print("OUT ", num)
+	if cab_state == num:
+		cab_state = null
